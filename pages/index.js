a377afa8961b82c5e7a2ffcb1a0ff6a1ebe0dfc2
Jacob Truong <jacob.truong@ymail.com>
2023-02-16 08:24:13 -0800

    jsx

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import { MainBanner } from "../components/main/MainBanner";
-import OclockIcon from "../components/main/OclockIcon";
+import OclockIcon from "../components/main/OclockIcon.jsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
