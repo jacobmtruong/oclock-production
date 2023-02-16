8dceb7d7da5c8f2237c6f3d229fe4065e93eacbe
Jacob Truong <jacob.truong@ymail.com>
2023-02-16 11:03:47 -0800

    appt

--- components/main/OclockIcon.jsx
+++ components/main/OclockIcon.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import icon from "../../styles/images/circleicon.png";
 import classes from "../../styles/mainpage/oclockIcon.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 const OclockIcon = () => {
   return (
     <div className={classes.container}>
+      <Link href="" className={classes.button}>
+        Book an Appointment
+      </Link>
       <Image src={icon} className={classes.image} />
     </div>
   );
