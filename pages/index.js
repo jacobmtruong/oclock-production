07a8db7811c2a61feb795df742fd3187ba6e5070
Jacob Truong <jacob.truong@ymail.com>
2023-02-23 11:26:13 -0800

    head

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import { Inter } from "@next/font/google";
+import Head from "next/head";
 import { MainBanner } from "../components/main/MainBanner";
+import icon from "../styles/images/circleicon.png";
 import { LandscapeCarousel } from "../components/sliders/LandscapeCarousel.jsx";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>O'CLOCK PRODUCTION</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <MainBanner />
       <LandscapeCarousel />
     </>
