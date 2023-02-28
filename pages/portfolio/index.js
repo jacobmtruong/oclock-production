8fcc26c206cf20e8889fd0180c9e01bf8c734e66
Jacob Truong <jacob.truong@ymail.com>
2023-02-28 15:02:05 -0800

    commit head

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -2,10 +2,14 @@ import { MainBanner } from "../../components/main/MainBanner";
 import DisplayImages from "../../components/portfolio/DisplayImages";
 import ImageBannerPortfolio from "../../components/portfolio/ImageBannerPortfolio";
 import Footer from "../../components/main/Footer";
+import Head from "next/head";
 
 export default function Portfolio() {
   return (
     <div>
+      <Head>
+        <title>PORTFOLIO | O'CLOCK</title>
+      </Head>
       <MainBanner />
       <ImageBannerPortfolio />
       <DisplayImages />
