20a834d32ac5338f001b3fe3179ec7b66dff7563
Jacob Truong <jacob.truong@ymail.com>
2023-02-28 10:53:11 -0800

    commit

--- components/portfolio/DisplayImages.jsx
+++ components/portfolio/DisplayImages.jsx
@@ -1,26 +1,24 @@
 import { getAllImage } from "../../data/images";
 import Fade from "react-reveal/Fade";
 import classes from "../../styles/portfoliostyles/displayimages.module.css";
-import React, { useState } from "react";
 import ModalImage from "react-modal-image";
 
 const DisplayImages = () => {
   const data = getAllImage();
 
   return (
-    <Fade>
-      <div className={classes.container}>
-        {data.map((image, i) => (
-          <ModalImage
-            small={image.url}
-            large={image.url}
-            className={classes.card}
-            alt={image.content}
-            hideDownload="true"
-          />
-        ))}
-      </div>
-    </Fade>
+    <div className={classes.container}>
+      {data.map((image, i) => (
+        <ModalImage
+          small={image.url}
+          large={image.url}
+          className={classes.card}
+          alt={image.content}
+          hideDownload="true"
+          key={i}
+        />
+      ))}
+    </div>
   );
 };
 
