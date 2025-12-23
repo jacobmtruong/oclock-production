3393b9e2c3f08ab24e14debaf01a95b5f708a4df
Jacob Truong <jacob.truong@ymail.com>
2025-12-23 11:12:24 +0700

    css

--- components/main/OclockIcon.jsx
+++ components/main/OclockIcon.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import Image from "next/image";
 import icon from "../../styles/images/circleicon.png";
 import classes from "../../styles/mainpage/oclockIcon.module.css";
-import Image from "next/image";
 
 const OclockIcon = () => {
   return (
     <div className={classes.container}>
       <p className={classes.text}>PR</p>
-      <Image src={icon} className={classes.image} alt="brand-icon" />
+
+      <div className={classes.iconWrap}>
+        <Image
+          src={icon}
+          alt="brand-icon"
+          fill
+          priority
+          sizes="(max-width: 480px) 40vw, (max-width: 800px) 25vw, 8vh"
+          className={classes.image}
+        />
+      </div>
+
       <p className={classes.text}>DUCTION</p>
     </div>
   );
