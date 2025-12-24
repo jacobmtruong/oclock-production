dec3f6bd2a450474539f4fd9df9bb49335ecf9df
Jacob Truong <jacob.truong@ymail.com>
2025-12-24 22:42:10 +0700

    admin thumbnails

--- models/Photo.js
+++ models/Photo.js
@@ -5,9 +5,20 @@ const PhotoSchema = new mongoose.Schema(
     legacyId: Number,
     url: { type: String, required: true },
     content: { type: String, default: "" },
-    category: { type: String, required: true },
-    view: { type: String, required: true },
     favorite: { type: Boolean, default: false },
+
+    category: {
+      type: String,
+      required: true,
+      enum: ["food", "beverage", "product", "architecture", "lifestyle"],
+    },
+    view: { type: String, required: true, enum: ["landscape", "portrait"] },
+
+    boardKey: {
+      type: String,
+      enum: ["fnb", "product", "architecture", "lifestyle", null],
+      default: null,
+    },
   },
   { timestamps: true }
 );
