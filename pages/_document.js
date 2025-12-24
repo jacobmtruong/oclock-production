68dccacc2567da2efa87deb98eeca3b0678849d5
Jacob Truong <jacob.truong@ymail.com>
2025-12-24 23:20:42 +0700

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Raleway:wght@200;400&display=swap"
           rel="stylesheet"
